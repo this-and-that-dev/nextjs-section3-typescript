a78230627139e791ef97abb9302f000bfa27315f
공수한 <ksh92315@pionnet.co.kr>
2025-11-01 22:28:56 +0900

    35. 3.5) 리액트 서버 컴포넌트 주의사항

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,5 +1,13 @@
-import styles from "./page.module.css";
+import ClientComponent from "@/app/(with-searchbar)/client-component";
+import ServerComponent from "@/app/(with-searchbar)/server-component";
 
 export default function Home() {
-  return <div className={styles.page}>인덱스 페이지</div>;
+  return (
+    <div>
+      인덱스 페이지
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
+    </div>
+  );
 }
---
+++ src/app/(with-searchbar)/server-component.tsx
@@ -0,0 +1,4 @@
+export default function ServerComponent() {
+  console.log("Server Component");
+  return <div></div>;
+}
